dd2ea5785ed48d1cea359defddb9755f83de0cda
Marc Vila <marcvilagomez@gmail.com>
2018-02-10 18:53:52 +0100

    Adding map to Android app

--- android/app/src/main/java/com/fishhackathon/hackathon/fishhackathon/MapFragment.java
+++ android/app/src/main/java/com/fishhackathon/hackathon/fishhackathon/MapFragment.java
@@ -1,18 +1,48 @@
 package com.fishhackathon.hackathon.fishhackathon;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.MapTileProviderBasic;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.TilesOverlay;
+
 public class MapFragment extends Fragment {
     private View rootView;
-    private TextView title;
+    private TextView zoomInView;
+    private TextView zoomOutView;
+    private TextView legendView;
+    private MapView osmMap;
 
     public static MapFragment newInstance() {
         return new MapFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Configuration.getInstance().load(getContext(), PreferenceManager.getDefaultSharedPreferences(getContext()));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Configuration.getInstance().load(getContext(), PreferenceManager.getDefaultSharedPreferences(getContext()));
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,12 +50,69 @@ public class MapFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
         setUpElements();
-        title.setText(getString(R.string.title_map));
+        setUpListeners();
+
+        osmMap.setMultiTouchControls(true);
+        addOverlay();
+
+        //Geopoint located in Cadiz - Campus Puerto Real
+        centerMap(new GeoPoint(36.528869, -6.213082));
 
         return rootView;
     }
 
+    private void showLegend() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext());
+        dialogBuilder.setView(getLayoutInflater().inflate(R.layout.map_legend_alert, null));
+        dialogBuilder.create();
+        dialogBuilder.show();
+    }
+
+    private void addOverlay() {
+        MapTileProviderBasic mProvider = new MapTileProviderBasic(getContext());
+        TilesOverlay seaMap = new TilesOverlay(mProvider, getContext());
+        seaMap.setLoadingLineColor(Color.TRANSPARENT);
+        seaMap.setLoadingBackgroundColor(Color.TRANSPARENT);
+        seaMap.setLoadingDrawable(null);
+        mProvider.setTileSource(TileSourceFactory.OPEN_SEAMAP);
+        osmMap.getOverlays().add(seaMap);
+        osmMap.postInvalidate();
+    }
+
+    private void centerMap(GeoPoint geoPoint) {
+        IMapController mapController = osmMap.getController();
+        mapController.setZoom(15);
+        mapController.setCenter(geoPoint);
+    }
+
     private void setUpElements() {
-        title = (TextView) rootView.findViewById(R.id.map_fragment_title);
+        osmMap = (MapView) rootView.findViewById(R.id.osmap);
+        zoomInView = (TextView) rootView.findViewById(R.id.home_zoom_in);
+        zoomOutView = (TextView) rootView.findViewById(R.id.home_zoom_out);
+        legendView = (TextView) rootView.findViewById(R.id.home_legend);
+    }
+
+    private void setUpListeners() {
+        zoomInView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (osmMap != null && osmMap.canZoomIn()) {
+                    osmMap.getController().setZoom(osmMap.getZoomLevel() + 1);
+                }
+            }
+        });
+
+        zoomOutView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (osmMap != null && osmMap.canZoomOut()) {
+                    osmMap.getController().setZoom(osmMap.getZoomLevel() - 1);
+                }
+            }
+        });
+
+        legendView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showLegend();
+            }
+        });
     }
 }
