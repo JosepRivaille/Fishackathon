47ea669dff253edad8ff5c56a96dc780b2f9bd2e
elorenteg <ester.lorente@est.fib.upc.edu>
2018-02-11 01:23:52 +0100

    coordinate inside polygon

--- backend/index.js
+++ backend/index.js
@@ -35,59 +35,33 @@ app.use('/graphiql', graphiqlExpress({
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
 
 app.get('/zones', (req, res) => {
-	insert_zones();
+	//insertZones();
+	insidePolygon();
 	res.send("OK");
 });
 
+
+
+import inside from 'point-in-polygon';
 import ZoneModel from './src/zones/model';
 
-function insert_zones() {
-	var fs = require('fs');
-	var json = JSON.parse(fs.readFileSync('src/zones/FAO_AREAS/FAO_AREAS.json', 'utf8'));
-	var features = json["features"];
+function insidePolygon() {//lat, lng) {
+	//var coordinates = [lat, lng];
 
-	for(var i = 0; i < features.length; i++) {
-		var properties = features[i]["properties"];
-		var code = properties["F_CODE"];
-		var level = properties["F_LEVEL"];
-		var ocean = properties["OCEAN"];
-		var parent = "";
-		if (level === "SUBAREA") parent = properties["F_AREA"];
-		else if (level === "DIVISION") parent = properties["F_SUBAREA"];
-		else if (level === "SUBDIVISION") parent = properties["F_DIVISION"];
-		else if (level === "SUBUNIT") parent = properties["F_SUBDIVIS"];
+	var result = ZoneModel.findOne( { 'code': '21' } );
 
-		var geometry = features[i]["geometry"];
-		var coordinates = [];
-		geometry.coordinates.forEach(function(polygon){
-			var polygoncoords = [];
-		   	polygon.forEach(function(linearring){
-				var firstring = linearring[0];
-				var latlong = {
-			   		lat: firstring[0],
-	        		lng: firstring[1]
-	    		}
-			   	polygoncoords.push(latlong);
-			});
-			coordinates.push(polygoncoords);
-		});
+	/*
+	var zones = [];
+	var zoneinsideId = -1;
+	zones.forEach(function(zone){
+		zone.polygon.forEach(function(polygon){
+			var isInside = inside(coordinates, polygon);
+			if (isInside) zoneinsideId = zone.code;
+		})
+	})
+	*/
 
-		create_zone(code, level, ocean, parent, coordinates);
-	}
-}
-
-function create_zone(code, level, ocean, parent, polygon) {
-	
-	const zone = new ZoneModel({
-                id: undefined,
-                code,
-                level,
-                ocean: ocean.toUpperCase(),
-                parent,
-                polygon: polygon,
-                laws: []
-            });
-    zone.save();
+	return result;
 }
 
 
