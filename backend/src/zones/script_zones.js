cad8bbaa4df53b5dafecd6650119c935e37a12d1
elorenteg <ester.lorente@est.fib.upc.edu>
2018-02-11 01:57:59 +0100

    Compute zone centroids

--- backend/src/zones/script_zones.js
+++ backend/src/zones/script_zones.js
@@ -18,6 +18,7 @@ function insertZones() {
 
 		var geometry = features[i]["geometry"];
 		var coordinates = [];
+		var centroids = [];
 		geometry.coordinates.forEach(function(polygon){
 			var polygoncoords = [];
 		   	polygon.forEach(function(linearring){
@@ -29,13 +30,21 @@ function insertZones() {
 			   	polygoncoords.push(latlong);
 			});
 			coordinates.push(polygoncoords);
-		});
 
-		createZone(code, level, ocean, parent, coordinates);
+			var centerpolygon = computeCenterOfPolygon(polygoncoords);
+			centroids.push(centerpolygon);
+		});
+		var centroidLat = centroids.reduce((a, o, i, p) => a + o.lat / p.length, 0);
+		var centroidLng = centroids.reduce((a, o, i, p) => a + o.lng / p.length, 0);
+		var centroid = {lat: centroidLat, lng: centroidLng}
+		console.log(centroid);
+		console.log(centroids);
+
+		createZone(code, level, ocean, parent, coordinates, centroid);
 	}
 }
 
-function createZone(code, level, ocean, parent, polygon) {
+function createZone(code, level, ocean, parent, polygon, centroid) {
 	
 	const zone = new ZoneModel({
                 id: undefined,
@@ -44,7 +53,22 @@ function createZone(code, level, ocean, parent, polygon) {
                 ocean: ocean.toUpperCase(),
                 parent,
                 polygon: polygon,
+                centroid,
                 laws: []
             });
     zone.save();
+}
+
+function computeCenterOfPolygon(polygon) {
+	var maxLat = Math.max.apply(Math,polygon.map(function(o){return o.lat;}));
+	var minLat = Math.min.apply(Math,polygon.map(function(o){return o.lat;}));
+	var maxLng = Math.max.apply(Math,polygon.map(function(o){return o.lng;}));
+	var minLng = Math.min.apply(Math,polygon.map(function(o){return o.lng;}));
+
+	var center = {
+		lat: minLat+(maxLat-minLat)/2,
+		lng: minLng+(maxLng-minLng)/2
+	}
+
+	return center;
 }
